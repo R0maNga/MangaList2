b8b6c6eeccf1987129f1e87187bcd5101e80cfbc
R0maNga <R0maNga@users.noreply.github.com>
2022-11-05 16:07:07 +0300

    all

---
+++ app/src/main/java/com/example/mangalist/model/DbManga.kt
@@ -0,0 +1,11 @@
+package com.example.mangalist.model
+
+import com.google.gson.annotations.SerializedName
+
+data class DbManga(
+    var id: String,
+    var images: String?,
+    var title: String?,
+    var rank: String?,
+    //var watchStatus: String?
+)
